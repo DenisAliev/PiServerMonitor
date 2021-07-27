149d8114b0903c883fca782c561334a799e4c2f7
DenisAliev <aliev372001@gmail.com>
2021-07-27 18:33:36 +0300

    Added JwtService

---
+++ PiServerMonitor/Services/Authentication/JwtService.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PiServerMonitor.Services.Authentication
+{
+    public class JwtService: IJwtService
+    {
+        private readonly int _hours;
+        private readonly SymmetricSecurityKey _securityKey;
+        public JwtService(string key, int hours)
+        {
+            _securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key));;
+            _hours = hours;
+        }
+        public JwtResponse GetToken(JwtUser user)
+        {
+            var identity = GetIdentity(user);
+            var now = DateTime.UtcNow;
+            var jwt = new JwtSecurityToken(
+                issuer: JwtOptions.ISSUER,
+                audience: JwtOptions.AUDIENCE,
+                notBefore: now,
+                claims: identity.Claims,
+                expires: now.Add(TimeSpan.FromHours(_hours)),
+                signingCredentials: new SigningCredentials(_securityKey, SecurityAlgorithms.Sha256)
+            );
+            var encodedToken = new JwtSecurityTokenHandler().WriteToken(jwt);
+            return new JwtResponse(encodedToken);
+        }
+        private ClaimsIdentity GetIdentity(JwtUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login)
+            };
+            ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+            return claimsIdentity;
+        }
+        
+    }
+}

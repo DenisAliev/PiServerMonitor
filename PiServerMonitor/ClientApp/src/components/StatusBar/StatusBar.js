96c0cd661673a5429a6a6ec09da32ed46542cad0
DenisAliev <aliev372001@gmail.com>
2021-07-31 17:09:10 +0300

    ended StatusBar

--- PiServerMonitor/ClientApp/src/components/StatusBar/StatusBar.js
+++ PiServerMonitor/ClientApp/src/components/StatusBar/StatusBar.js
@@ -1,22 +1,36 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Row, Col, Card, CardTitle } from "reactstrap";
 import "./StatusBar.css"
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import webAPI from "../../services/webAPI.service"
 
-export default function StatusBar() {
-    const [infoStatuses, setInfoStatuses] = useState([{text:"Test",color:"info",progress:25},
-        {text:"Test",color:"info",progress:25}]);
+export default function StatusBar(props) {
+    const [Stats, setStats] = useState([]);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            webAPI.getInfo().then( (res) => {
+                if(res!==null){
+                    setStats(res);
+                }else{
+                    if(props.setIsAuth!==undefined) {
+                        props.setIsAuth(false);
+                    }
+                }
+            });
+        }, 500);
+        return () => clearInterval(interval);
+    }, []);
     
     return(
             <div>
                 <Row className="status-bar-row" style={{justifyContent:"center"}}>
-                {infoStatuses.map( (infoStatus,i) =>{
+                {Stats.map( (Status, i) =>{
                     return(
                             <Col sm="3">
                                 <Card body s>
-                                    <CardTitle tag="h1" style={{textAlign:"center"}}>{infoStatus.text}</CardTitle>
-                                    <CircularProgressbar value={infoStatus.progress} text={`${infoStatus.progress}%`}/>
+                                    <CardTitle tag="h1" style={{textAlign:"center"}}>{Status.text}</CardTitle>
+                                    <CircularProgressbar value={Status.value} text={`${Status.value}%`}/>
                                 </Card>
                             </Col>
                     );

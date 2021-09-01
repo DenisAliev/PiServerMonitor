62d6df932a4b4a675ebdbc81561ad652e482c0b9
Denis <denis.aliev.home@gmail.com>
2021-09-01 16:49:52 +0300

    Added automatic checking for temperature

--- PiServerMonitor/Services/RaspberryPi/RpiExtensions.cs
+++ PiServerMonitor/Services/RaspberryPi/RpiExtensions.cs
@@ -1,12 +1,14 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace PiServerMonitor.Services.RaspberryPi
 {
     public static class RpiExtensions
     {
-        public static void AddRpiService(this IServiceCollection services)
+        public static void AddRpiService(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddTransient<IRpiService, RpiService>();
+            services.AddSingleton<IRpiService, RpiService>(provider => new RpiService(configuration));
         } 
     }
 }

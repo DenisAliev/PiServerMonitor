88b4b17ab51f61f35911d4e803e6279482f40076
Denis <denis.aliev.home@gmail.com>
2021-09-01 16:50:43 +0300

    Refactored JwtExceptions, Startup, Satus

--- PiServerMonitor/Services/Authentication/JwtExtensions.cs
+++ PiServerMonitor/Services/Authentication/JwtExtensions.cs
@@ -1,13 +1,15 @@
 using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace PiServerMonitor.Services.Authentication
 {
     public static class JwtExtensions
     {
-        public static void  AddJwtService(this IServiceCollection services, string key, int time)
+        public static void  AddJwtService(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddTransient<IJwtService, JwtService>(provider => new JwtService(key, time));
+            services.AddTransient<IJwtService, JwtService>(provider => new JwtService(configuration["JwtToken:Key"],
+                int.Parse(configuration["JwtToken:Lifetime"])));
         }
     }
 }

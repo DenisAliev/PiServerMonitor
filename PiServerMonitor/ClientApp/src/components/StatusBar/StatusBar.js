0f61815c5f328f7fc310d30485a086af63e96129
Denis <denis.aliev.home@gmail.com>
2021-09-02 19:27:29 +0300

    Refactored StatusBar

--- PiServerMonitor/ClientApp/src/components/StatusBar/StatusBar.js
+++ PiServerMonitor/ClientApp/src/components/StatusBar/StatusBar.js
@@ -1,32 +1,16 @@
-import React, {useEffect, useState} from "react";
-import {Row, Col, Card, CardTitle } from "reactstrap";
+import React from "react";
+import {Row} from "reactstrap";
 import "./StatusBar.css"
-import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import webAPI from "../../services/webAPI.service"
 import ProgressBar from "../ProgressBar";
 
+
 export default function StatusBar(props) {
-    const [Stats, setStats] = useState([]);
-    useEffect(() => {
-        const interval = setInterval(() => {
-            webAPI.getInfo().then( (res) => {
-                if(res!==null){
-                    setStats(res);
-                }else{
-                    if(props.setIsAuth!==undefined) {
-                        props.setIsAuth(false);
-                    }
-                }
-            });
-        }, 500);
-        return () => clearInterval(interval);
-    }, []);
     
     return(
             <div>
-                <Row className="status-bar-row" style={{justifyContent:"center"}}>
-                {Stats.map( (status, i) =>{
+                <Row className="status-bar-row" style={{justifyContent:"left"}}>
+                {props.stats.map( (status, i) =>{
                     return(
                         <ProgressBar value={status.value} text={status.text} unit={status.unit}/>
                     );
